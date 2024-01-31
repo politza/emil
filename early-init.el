4ddb9b6754aec653d4d44a688ece63862d1db843
Andreas Politz <andreas.politz@dedalus.com>
2024-01-31 18:05:14 +0100

    start work on the Core package

--- early-init.el
+++ early-init.el
@@ -1,4 +1,11 @@
 ;; -*- lexical-binding: t -*-
 
-;; Disable package, since we use straight.
 (setq package-enable-at-startup nil)
+
+(defconst Core:user-directory (concat user-emacs-directory "user")
+  "The user directory of Blue.
+
+This directory may contain custom init-files.")
+
+(when (file-exists-p (expand-file-name "early-init.el" Core:user-directory))
+  (load-file (expand-file-name "early-init.el" Core:user-directory)))
