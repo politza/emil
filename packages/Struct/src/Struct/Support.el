06b68acabc9fe22388833eff0b89cfc5956f4994
Andreas Politz <mail@andreas-politz.de>
2024-11-23 22:16:16 +0100

    make self have constant face

--- packages/Struct/src/Struct/Support.el
+++ packages/Struct/src/Struct/Support.el
@@ -17,7 +17,7 @@
      (2 font-lock-type-face nil t)
      (3 font-lock-type-face nil t))
     ("\\(\\_<self\\)\\(?:\\.?\\|\\_>\\)"
-     (1 font-lock-keyword-face))))
+     (1 font-lock-constant-face))))
 
 (define-minor-mode Struct:Support:syntax-mode
   "Adds support for better syntaxing of structs."
