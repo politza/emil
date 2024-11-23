259201e144f732f848b4f424a9864f8e9e42689a
Andreas Politz <mail@andreas-politz.de>
2024-11-23 22:16:16 +0100

    extend typings and add ideas to Rs

--- packages/Rs/src/Rs.el
+++ packages/Rs/src/Rs.el
@@ -14,19 +14,25 @@
 ;; See https://github.com/reactive-streams/reactive-streams-jvm/blob/v1.0.4/README.md#specification 
 
 (Trait:define Rs:Publisher ()
-  (defmethod Rs:Publisher:subscribe (self subscriber)))
+  (defmethod Rs:Publisher:subscribe (self (subscriber (Trait Rs:Subscriber)))))
 
 (Trait:define Rs:Subscriber ()
-  (defmethod Rs:Subscriber:on-subscribe (self subscription))
+  (defmethod Rs:Subscriber:on-subscribe (self (subscription (Trait Rs:Subscription))))
   (defmethod Rs:Subscriber:on-next (self value))
-  (defmethod Rs:Subscriber:on-error (self error))
+  (defmethod Rs:Subscriber:on-error (self (error Rs:error)))
   (defmethod Rs:Subscriber:on-complete (self)))
 
 (Trait:define Rs:Subscription ()
-  (defmethod Rs:Subscription:request (self amount))
+  (defmethod Rs:Subscription:request (self (amount (integer 1 *))))
   (defmethod Rs:Subscription:cancel (self)))
 
 (Trait:define Rs:Processor (Rs:Publisher Rs:Subscriber))
+
+(cl-deftype Rs:error ()
+  `(and cons
+        (satisfies (lambda (value)
+                     (and (symbolp (car value))
+                          (get (car value) 'error-conditions))))))
 
 (provide 'Rs)
 ;;; Rs.el ends here
