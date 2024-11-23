6eedd737308bf0d259974badeff699bcce6bfd98
Andreas Politz <mail@andreas-politz.de>
2024-11-23 22:16:16 +0100

    implemented a context for inference

---
+++ packages/Emil/src/Emil/Context.el
@@ -0,0 +1,160 @@
+;; -*- lexical-binding: t -*-
+
+(require 'Emil/Type)
+(require 'Commons)
+(require 'dash)
+(require 'Struct)
+(require 'Struct/Pcase)
+
+(Struct:define Emil:Context
+  "Provides a context for type-inference.
+
+The entries of the context are organized as a stack, i.e. entries are
+added and removed from the left."
+  (entries :type list))
+
+(Struct:define Emil:Context:Binding
+  "Maps a variable to some type.
+
+The term variable refers to a programmer's variable, i.e. not a
+type-variable. Note, that type may be incomplete: It may refer to yet
+to be solved type-variables."
+  (variable :type symbol)
+  (type :type (Trait Emil:Type)))
+
+(Struct:define Emil:Context:Marker
+  "Defines a marker establishing a before and after context.
+
+The marker contains an arbitrary token value, in order to
+differentiate this marker from other markers. Though, in the current
+implementation it is always a value of type `Emil:Type:VarInst'."
+  (token))
+
+(Struct:define Emil:Context:SolvedVarInst
+  "Maps an instance of a variable-type to some type.
+
+During type-inference, instances of type-variables of polymorphic
+types are resolved to other types. This struct keeps track of these
+assignments."
+  (variable :type Emil:Type:VarInst)
+  (type :type (Trait Emil:Type)))
+
+(Struct:implement Emil:Context
+  (fn Emil:Context:hole (self (variable Emil:Type:VarInst))
+    "Splits this context at VARIABLE.
+
+Returns a cons of new contexts \(LEFT-CONTEXT . RIGHT-CONTEXT\) representing
+the entries left resp. right of VARIABLE, which itself is not
+included in neither.
+
+Returns `nil', if VARIABLE is not a member of this context."
+    (when-let (tail (member variable (Struct:get self :entries)))
+      (cons (Emil:Context :entries
+                          (--take-while (not (equal variable it))
+                                        (Struct:get self :entries)))
+            (Emil:Context :entries (cdr tail)))))
+
+  (fn Emil:Context:double-hole (self (variable Emil:Type:VarInst)
+                                     (other Emil:Type:VarInst))
+    "Splits this context at VARIABLE and OTHER.
+
+This works similar to `Emil:Context:hole', except that the entries are
+split twice. Returns a triple of 3 new contexts \(LEFT MID RIGHT\),
+given that the entries of this context look like \(LEFT (VARIABLE) MID
+(OTHER) RIGHT\) appended together.
+
+Returns `nil', if either VARIABLE or OTHER is not a member of this
+context; or if OTHER appears in front of VARIABLE."
+    (-when-let* ((left (Emil:Context:hole self variable))
+                 (right (Emil:Context:hole (cdr left) other)))
+      (list (car left) (car right) (cdr right))))
+
+  (fn Emil:Context:lookup-binding (self (variable symbol) -> (Trait Emil:Type))
+    "Lookup VARIABLE in this context.
+
+Returns variable's type; or `nil', if VARIABLE is not bound in this
+context."
+    (-some-> (--find (pcase it
+                       ((Struct Emil:Context:Binding :variable other)
+                        (equal variable other)))
+                     (Struct:get self :entries))
+      (Struct:get :type)))
+
+  (fn Emil:Context:lookup-solved (self (variable Emil:Type:VarInst)
+                                       -> (Trait Emil:Type))
+    "Lookup the instantiated type VARIABLE in this context.
+
+Returns variable's type; or `nil', if VARIABLE is not bound in this
+context."
+    (-some-> (--find (pcase it
+                       ((Struct Emil:Context:SolvedVarInst :variable other)
+                        (equal (Struct:get variable :name)
+                               (Struct:get other :name))))
+                     (Struct:get self :entries))
+      (Struct:get :type)))
+
+  (fn Emil:Context:drop-until-after (self entry)
+    "Remove entries up-to and including the given ENTRY.
+
+Returns an empty context, if ENTRY is not present in this one."
+    (Emil:Context
+     :entries
+     (cdr (--drop-while (not (equal it entry))
+                        (Struct:get self :entries)))))
+
+  (fn Emil:Context:well-formed-type? (self (type (Trait Emil:Type)))
+    "Returns non-nil, if TYPE is well-formed given this context."
+    (let ((entries (Struct:get self :entries)))
+      (pcase-exhaustive type
+        ((or (Struct Emil:Type:Any)
+             (Struct Emil:Type:Never)
+             (Struct Emil:Type:Null)
+             (Struct Emil:Type:Void)
+             (Struct Emil:Type:Basic))
+         t)
+        ((Struct Emil:Type:Var)
+         (not (null (member type entries))))
+        ((Struct Emil:Type:VarInst)
+         (not (null (or (member type entries)
+                        (Emil:Context:lookup-solved self type)))))
+        ((Struct Emil:Type:Fn argument-types rest-type return-type)
+         (and (--every? (Emil:Context:well-formed-type? self it)
+                        argument-types)
+              (Emil:Context:well-formed-type? self return-type)
+              (or (null rest-type)
+                  (Emil:Context:well-formed-type? self rest-type))))
+        ((Struct Emil:Type:Forall variables type)
+         (Emil:Context:well-formed-type?
+          (Emil:Context :entries (append variables entries nil))
+          type)))))
+
+  (fn Emil:Context:resolve (self (type (Trait Emil:Type)))
+    "Resolves the given TYPE against this context.
+
+This will substitute any solved instantiated type-variables with their
+resolved type. This applies recursively; both to the TYPE and its
+unresolved type-variables."
+    (pcase-exhaustive type
+      ((or (Struct Emil:Type:Any)
+           (Struct Emil:Type:Never)
+           (Struct Emil:Type:Null)
+           (Struct Emil:Type:Void)
+           (Struct Emil:Type:Basic)
+           (Struct Emil:Type:Var))
+       type)
+      ((Struct Emil:Type:VarInst)
+       (if-let (resolved (Emil:Context:lookup-solved self type))
+           (Emil:Context:resolve self resolved)
+         type))
+      ((Struct Emil:Type:Fn argument-types rest-type return-type)
+       (let ((argument-types
+              (--map (Emil:Context:resolve self it) argument-types))
+             (rest-type (if rest-type (Emil:Context:resolve self rest-type)))
+             (return-type (Emil:Context:resolve self return-type)))
+         (Emil:Type:Fn* ,@type argument-types rest-type return-type)))
+      ((Struct Emil:Type:Forall variables type)
+       (Emil:Type:Forall*
+        variables
+        :type (Emil:Context:resolve self type))))))
+
+(provide 'Emil/Context)
---
+++ packages/Emil/test/Emil/Context-test.el
@@ -0,0 +1,418 @@
+;; -*- lexical-binding: t -*-
+
+(require 'buttercup)
+(require 'Emil/Context)
+
+(describe "Emil:Context"
+  (describe "Emil:Context:hole"
+    (it "empty"
+      (expect (Emil:Context:hole (Emil:Context)
+                                 (Emil:Type:VarInst :name 'a))
+              :to-equal nil))
+
+    (it "singleton / hit"
+      (expect (Emil:Context:hole
+               (Emil:Context
+                :entries (list (Emil:Type:VarInst :name 'a)))
+               (Emil:Type:VarInst :name 'a))
+              :to-equal (cons (Emil:Context)
+                              (Emil:Context))))
+
+    (it "singleton / miss"
+      (expect (Emil:Context:hole
+               (Emil:Context
+                :entries (list (Emil:Type:VarInst :name 'a)))
+               (Emil:Type:VarInst :name 'b))
+              :to-equal nil))
+
+    (it "triple / hit"
+      (expect (Emil:Context:hole
+               (Emil:Context
+                :entries (list (Emil:Type:VarInst :name 'a)
+                               (Emil:Type:VarInst :name 'b)
+                               (Emil:Type:VarInst :name 'c)))
+               (Emil:Type:VarInst :name 'b))
+              :to-equal (cons
+                         (Emil:Context
+                          :entries (list (Emil:Type:VarInst :name 'a)))
+                         (Emil:Context
+                          :entries (list (Emil:Type:VarInst :name 'c))))))
+
+    (it "triple / miss"
+      (expect (Emil:Context:hole
+               (Emil:Context
+                :entries (list (Emil:Type:VarInst :name 'a)
+                               (Emil:Type:VarInst :name 'b)
+                               (Emil:Type:VarInst :name 'c)))
+               (Emil:Type:VarInst :name 'd))
+              :to-equal nil)))
+
+  (describe "Emil:Context:double-hole"
+    (it "empty"
+      (expect (Emil:Context:double-hole
+               (Emil:Context)
+               (Emil:Type:VarInst :name 'a)
+               (Emil:Type:VarInst :name 'b))
+              :to-equal nil))
+
+    (it "singleton"
+      (expect (Emil:Context:double-hole
+               (Emil:Context
+                :entries (list (Emil:Type:VarInst :name 'a)))
+               (Emil:Type:VarInst :name 'a)
+               (Emil:Type:VarInst :name 'b))
+              :to-equal nil))
+
+    (it "triple / miss"
+      (expect (Emil:Context:double-hole
+               (Emil:Context
+                :entries (list (Emil:Type:VarInst :name 'a)
+                               (Emil:Type:VarInst :name 'b)
+                               (Emil:Type:VarInst :name 'c)))
+               (Emil:Type:VarInst :name 'a)
+               (Emil:Type:VarInst :name 'd))
+              :to-equal nil))
+
+    (it "triple / hit"
+      (expect (Emil:Context:double-hole
+               (Emil:Context
+                :entries (list (Emil:Type:VarInst :name 'a)
+                               (Emil:Type:VarInst :name 'b)
+                               (Emil:Type:VarInst :name 'c)))
+               (Emil:Type:VarInst :name 'a)
+               (Emil:Type:VarInst :name 'c))
+              :to-equal (list
+                         (Emil:Context)
+                         (Emil:Context
+                          :entries (list (Emil:Type:VarInst :name 'b)))
+                         (Emil:Context))))
+
+    (it "quintuple / miss"
+      (expect (Emil:Context:double-hole
+               (Emil:Context
+                :entries (list (Emil:Type:VarInst :name 'a)
+                               (Emil:Type:VarInst :name 'b)
+                               (Emil:Type:VarInst :name 'c)
+                               (Emil:Type:VarInst :name 'd)
+                               (Emil:Type:VarInst :name 'e)))
+               (Emil:Type:VarInst :name 'b)
+               (Emil:Type:VarInst :name 'f))
+              :to-equal nil))
+
+    (it "quintuple / hit"
+      (expect (Emil:Context:double-hole
+               (Emil:Context
+                :entries (list (Emil:Type:VarInst :name 'a)
+                               (Emil:Type:VarInst :name 'b)
+                               (Emil:Type:VarInst :name 'c)
+                               (Emil:Type:VarInst :name 'd)
+                               (Emil:Type:VarInst :name 'e)))
+               (Emil:Type:VarInst :name 'b)
+               (Emil:Type:VarInst :name 'd))
+              :to-equal (list
+                         (Emil:Context
+                          :entries (list (Emil:Type:VarInst :name 'a)))
+                         (Emil:Context
+                          :entries (list (Emil:Type:VarInst :name 'c)))
+                         (Emil:Context
+                          :entries (list (Emil:Type:VarInst :name 'e)))))))
+
+  (describe "Emil:Context:lookup-binding"
+    (it "empty"
+      (expect (Emil:Context:lookup-binding
+               (Emil:Context)
+               'a)
+              :to-equal nil))
+
+    (it "singleton / miss"
+      (expect (eval '(Emil:Context:lookup-binding
+                      (Emil:Context
+                       :entries (list (Emil:Context:Binding
+                                       :variable 'b
+                                       :type (Emil:Type:Never))))
+                      'a))
+              :to-equal nil))
+
+    (it "singleton / hit"
+      (expect (eval '(Emil:Context:lookup-binding
+                      (Emil:Context
+                       :entries (list (Emil:Context:Binding
+                                       :variable 'a
+                                       :type (Emil:Type:Never))))
+                      'a))
+              :to-equal (Emil:Type:Never)))
+
+    (it "triple / hit"
+      (expect (eval '(Emil:Context:lookup-binding
+                      (Emil:Context
+                       :entries (list (Emil:Context:Binding
+                                       :variable 'a
+                                       :type (Emil:Type:Any))
+                                      (Emil:Context:Binding
+                                       :variable 'b
+                                       :type (Emil:Type:Never))
+                                      (Emil:Type:VarInst :name 'c)))
+                      'b))
+              :to-equal (Emil:Type:Never)))
+
+    (it "triple / miss"
+      (expect (eval '(Emil:Context:lookup-binding
+                      (Emil:Context
+                       :entries (list (Emil:Type:VarInst :name 'a)
+                                      (Emil:Context:Binding
+                                       :variable 'b
+                                       :type (Emil:Type:Never))
+                                      (Emil:Type:VarInst :name 'c)))
+                      'a))
+              :to-equal nil)))
+
+  (describe "Emil:Context:lookup-solved"
+    (it "empty"
+      (expect (Emil:Context:lookup-solved
+               (Emil:Context)
+               (Emil:Type:VarInst :name 'a))
+              :to-equal nil))
+
+    (it "singleton / miss"
+      (expect (eval '(Emil:Context:lookup-solved
+                      (Emil:Context
+                       :entries (list (Emil:Context:SolvedVarInst
+                                       :variable (Emil:Type:VarInst :name 'b)
+                                       :type (Emil:Type:Never))))
+                      (Emil:Type:VarInst :name 'a)))
+              :to-equal nil))
+
+    (it "singleton / hit"
+      (expect (eval '(Emil:Context:lookup-solved
+                      (Emil:Context
+                       :entries (list (Emil:Context:SolvedVarInst
+                                       :variable (Emil:Type:VarInst :name 'a)
+                                       :type (Emil:Type:Never))))
+                      (Emil:Type:VarInst :name 'a)))
+              :to-equal (Emil:Type:Never)))
+
+    (it "triple / hit"
+      (expect (eval '(Emil:Context:lookup-solved
+                      (Emil:Context
+                       :entries (list (Emil:Context:SolvedVarInst
+                                       :variable (Emil:Type:VarInst :name 'a)
+                                       :type (Emil:Type:Any))
+                                      (Emil:Context:SolvedVarInst
+                                       :variable (Emil:Type:VarInst :name 'b)
+                                       :type (Emil:Type:Never))
+                                      (Emil:Type:VarInst :name 'c)))
+                      (Emil:Type:VarInst :name 'a)))
+              :to-equal (Emil:Type:Any)))
+
+    (it "triple / miss"
+      (expect (eval '(Emil:Context:lookup-solved
+                      (Emil:Context
+                       :entries (list (Emil:Type:VarInst :name 'a)
+                                      (Emil:Context:SolvedVarInst
+                                       :variable (Emil:Type:VarInst :name 'b)
+                                       :type (Emil:Type:Never))
+                                      (Emil:Type:VarInst :name 'c)))
+                      (Emil:Type:VarInst :name 'a)))
+              :to-equal nil)))
+
+  (describe "Emil:Context:drop-until-after"
+    (it "empty"
+      (expect (Emil:Context:drop-until-after
+               (Emil:Context)
+               (Emil:Type:Var :name 'a))
+              :to-equal (Emil:Context)))
+
+    (it "singleton"
+      (expect (Emil:Context:drop-until-after
+               (Emil:Context
+                :entries (list (Emil:Type:Var :name 'a)))
+               (Emil:Type:Var :name 'a))
+              :to-equal (Emil:Context)))
+
+    (it "triple / match beginning"
+      (expect (Emil:Context:drop-until-after
+               (Emil:Context
+                :entries (list (Emil:Type:Var :name 'a)
+                               (Emil:Type:Var :name 'b)
+                               (Emil:Type:Var :name 'c)))
+               (Emil:Type:Var :name 'a))
+              :to-equal (Emil:Context
+                         :entries (list (Emil:Type:Var :name 'b)
+                                        (Emil:Type:Var :name 'c)))))
+
+    (it "triple / match center"
+      (expect (Emil:Context:drop-until-after
+               (Emil:Context
+                :entries (list (Emil:Type:Var :name 'a)
+                               (Emil:Type:Var :name 'b)
+                               (Emil:Type:Var :name 'c)))
+               (Emil:Type:Var :name 'b))
+              :to-equal (Emil:Context
+                         :entries (list (Emil:Type:Var :name 'c)))))
+
+    (it "triple / match end"
+      (expect (Emil:Context:drop-until-after
+               (Emil:Context
+                :entries (list (Emil:Type:Var :name 'a)
+                               (Emil:Type:Var :name 'b)
+                               (Emil:Type:Var :name 'c)))
+               (Emil:Type:Var :name 'c))
+              :to-equal (Emil:Context))))
+
+  (describe "Emil:Context:well-formed-type?"
+    (describe "basic"
+      (it "Emil:Type:Basic"
+        (expect (Emil:Context:well-formed-type?
+                 (Emil:Context)
+                 (Emil:Type:Never))
+                :to-equal t))
+
+      (it "Emil:Type:Var / true"
+        (expect (Emil:Context:well-formed-type?
+                 (Emil:Context
+                  :entries (list (Emil:Type:Var :name 'a)))
+                 (Emil:Type:Var :name 'a))
+                :to-equal t))
+
+      (it "Emil:Type:Var / false"
+        (expect (Emil:Context:well-formed-type?
+                 (Emil:Context
+                  :entries (list (Emil:Type:Var :name 'a)))
+                 (Emil:Type:Var :name 'b))
+                :to-equal nil))
+
+      (it "Emil:Type:VarInst / true"
+        (expect (Emil:Context:well-formed-type?
+                 (Emil:Context
+                  :entries (list (Emil:Type:VarInst :name 'a)))
+                 (Emil:Type:VarInst :name 'a))
+                :to-equal t))
+
+      (it "Emil:Type:VarInst / false"
+        (expect (Emil:Context:well-formed-type?
+                 (Emil:Context
+                  :entries (list (Emil:Type:VarInst :name 'a)))
+                 (Emil:Type:VarInst :name 'b))
+                :to-equal nil))
+
+      (it "Emil:Type:VarInst / true via lookup"
+        (expect (eval '(Emil:Context:well-formed-type?
+                        (Emil:Context
+                         :entries (list (Emil:Context:SolvedVarInst
+                                         :variable (Emil:Type:VarInst :name 'a)
+                                         :type (Emil:Type:Never))))
+                        (Emil:Type:VarInst :name 'a)))
+                :to-equal t))
+
+      (it "Emil:Type:Fn / false"
+        (expect (eval '(Emil:Context:well-formed-type?
+                        (Emil:Context
+                         :entries (list (Emil:Context:SolvedVarInst
+                                         :variable (Emil:Type:VarInst :name 'a)
+                                         :type (Emil:Type:Never))))
+                        (Emil:Type:Fn
+                         :argument-types (list (Emil:Type:VarInst :name 'b))
+                         :return-type (Emil:Type:Basic :name 'number)
+                         :min-arity 1)))
+                :to-equal nil))
+
+      (it "Emil:Type:Fn / true"
+        (expect (eval '(Emil:Context:well-formed-type?
+                        (Emil:Context
+                         :entries (list (Emil:Context:SolvedVarInst
+                                         :variable (Emil:Type:VarInst :name 'a)
+                                         :type (Emil:Type:Never))))
+                        (Emil:Type:Fn
+                         :argument-types (list (Emil:Type:VarInst :name 'a))
+                         :return-type (Emil:Type:Basic :name 'number)
+                         :min-arity 1)))
+                :to-equal t)
+        )
+
+      (it "Emil:Type:Forall / false"
+        (expect (eval '(Emil:Context:well-formed-type?
+                        (Emil:Context
+                         :entries (list (Emil:Type:Var :name 'a)))
+                        (Emil:Type:Forall
+                         :variables (list (Emil:Type:Var :name 'b))
+                         :type (Emil:Type:Var :name 'c))))
+                :to-equal nil))
+
+      (it "Emil:Type:Forall / true"
+        (expect (eval '(Emil:Context:well-formed-type?
+                        (Emil:Context
+                         :entries (list (Emil:Type:Var :name 'a)))
+                        (Emil:Type:Forall
+                         :variables (list (Emil:Type:Var :name 'b))
+                         :type (Emil:Type:Var :name 'b))))
+                :to-equal t))))
+
+  (describe "Emil:Context:resolve"
+    (describe "basic"
+      (it "Emil:Type:Basic"
+        (expect (Emil:Context:resolve
+                 (Emil:Context)
+                 (Emil:Type:Never))
+                :to-equal (Emil:Type:Never)))
+
+      (it "Emil:Type:Var"
+        (expect (Emil:Context:resolve
+                 (Emil:Context)
+                 (Emil:Type:Var :name 'a))
+                :to-equal (Emil:Type:Var :name 'a)))
+
+      (it "Emil:Type:VarInst"
+        (expect (Emil:Context:resolve
+                 (Emil:Context)
+                 (Emil:Type:VarInst :name 'a))
+                :to-equal (Emil:Type:VarInst :name 'a)))
+
+      (it "Emil:Type:VarInst / via lookup"
+        (expect (eval '(Emil:Context:resolve
+                        (Emil:Context
+                         :entries (list (Emil:Context:SolvedVarInst
+                                         :variable (Emil:Type:VarInst :name 'a)
+                                         :type (Emil:Type:Never))))
+                        (Emil:Type:VarInst :name 'a)))
+                :to-equal (Emil:Type:Never)))
+
+      (it "Emil:Type:Fn"
+        (expect (eval '(Emil:Context:resolve
+                        (Emil:Context
+                         :entries (list (Emil:Context:SolvedVarInst
+                                         :variable (Emil:Type:VarInst :name 'a)
+                                         :type (Emil:Type:Any))
+                                        (Emil:Context:SolvedVarInst
+                                         :variable (Emil:Type:VarInst :name 'b)
+                                         :type (Emil:Type:Never))
+                                        (Emil:Context:SolvedVarInst
+                                         :variable (Emil:Type:VarInst :name 'c)
+                                         :type (Emil:Type:Void))))
+                        (Emil:Type:Fn
+                         :argument-types (list (Emil:Type:VarInst :name 'a))
+                         :rest-type (Emil:Type:VarInst :name 'c)
+                         :return-type (Emil:Type:VarInst :name 'b)
+                         :min-arity 1)))
+                :to-equal
+                (Emil:Type:Fn
+                 :argument-types (list (Emil:Type:Any))
+                 :rest-type (Emil:Type:Void)
+                 :return-type (Emil:Type:Never)
+                 :min-arity 1)))
+
+      (it "Emil:Type:ForAll"
+        (expect (eval '(Emil:Context:resolve
+                        (Emil:Context
+                         :entries (list (Emil:Context:SolvedVarInst
+                                         :variable (Emil:Type:VarInst :name 'a)
+                                         :type (Emil:Type:Any))
+                                        (Emil:Context:SolvedVarInst
+                                         :variable (Emil:Type:VarInst :name 'b)
+                                         :type (Emil:Type:Never))))
+                        (Emil:Type:Forall
+                         :variables (list (Emil:Type:Var :name 'a))
+                         :type (Emil:Type:VarInst :name 'b))))
+                :to-equal
+                (eval '(Emil:Type:Forall
+                        :variables (list (Emil:Type:Var :name 'a))
+                        :type (Emil:Type:Never))))))))
