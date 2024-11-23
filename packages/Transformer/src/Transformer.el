cbc22924da149d907d1711a74eda134c9c3988c2
Andreas Politz <mail@andreas-politz.de>
2024-11-23 22:16:18 +0100

    add license and license header

--- packages/Transformer/src/Transformer.el
+++ packages/Transformer/src/Transformer.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2024  Andreas Politz
 
-;; Author: Andreas Politz <mail@andreas-politz.de>
+;; Author: Andreas Politz
 ;; Keywords: extensions
 ;; Version: 1.0.0beta1
 ;; Package-Requires: ((emacs "29.1") (dash "2.19.1") (Struct "1.0.0beta1") (Commons "1.0.0beta1"))
