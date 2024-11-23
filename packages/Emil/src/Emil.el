ee660e3bedce5b147f351ace3761f4967c7c8732
Andreas Politz <mail@andreas-politz.de>
2024-11-23 22:16:16 +0100

    implemented a basic type-system

---
+++ packages/Emil/src/Emil.el
@@ -0,0 +1,13 @@
+;;; Emil.el --- Emacs mit Inferenz Logik.             -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Andreas Politz
+
+;; Author: Andreas Politz <mail@andreas-politz.de>
+;; Keywords: extensions
+;; Version: 1.0.0beta1
+;; Package-Requires: ((emacs "29.1") (dash "2.19.1") (Struct "1.0.0beta1") (Commons "1.0.0beta1"))
+
+(require 'Emil/Type)
+
+(provide 'Emil)
+;;; Emil.el ends here
