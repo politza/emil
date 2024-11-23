f4e71ec4ed1d357e205a4225ad4d15fd687d6834
Andreas Politz <mail@andreas-politz.de>
2024-11-23 22:16:16 +0100

    refactored documentation code into Doc.el

--- packages/Struct/src/Struct.el
+++ packages/Struct/src/Struct.el
@@ -11,6 +11,7 @@
 (require 'cl-macs)
 (require 'dash)
 (require 'Struct/Primitives)
+(require 'Struct/Doc)
 
 (defconst Struct:syntax-highlight-symbol 'Struct:syntax-highlight-symbol
   "Symbol to attach font-lock information to other symbols.")
@@ -18,11 +19,6 @@
 (defvar Struct:enable-syntax-highlighting t
   "Whether to highlight struct types with face `font-lock-type-face'.")
 
-;; These `defsubst' definitions need to be defined before they are
-;; used.
-
-
-;;;###autoload
 (defmacro Struct:define (name &optional documentation &rest declarations)
   "Defines a new struct-type named NAMẸ.
 
@@ -121,78 +117,7 @@ as splice-syntax.
    (t
     (error "Invalid property declaration: %s" declaration))))
 
-(defconst Struct:-doc-predicate
-  "Returns `t', if OBJECT is of struct-type `%s'.
 
-Otherwise returns nil, unless ENSURE is non-nil, in which a
-`wrong-type-argument' is signaled.")
-
-(defconst Struct:-doc-constructor-first-line
-  "Constructs a value of struct-type `%s'.")
-
-(defconst Struct:-doc-constructor-properties-line
-  "Struct `%s' has the following properties:")
-
-(defconst Struct:-doc-constructor-syntax-info
-  "The macro-version (with a star appended) of this constructor
-supports shorthand-syntax, i.e. keyword arguments may be omitted, when
-using an identifier eponymous with the property's name. And also
-spread-syntax via the `,@' operator, with other struct values as
-arguments.
-
-See also `%s'.")
-
-(defconst Struct:-doc-constructor-functions-info
-  "The following functions are associated with this struct:")
-
-(defun Struct:-doc-constructor (type)
-  (with-output-to-string
-    (princ (format Struct:-doc-constructor-first-line
-                   (Struct:unsafe-get type :name)))
-    (terpri nil t)
-    (terpri)
-    (when (Struct:unsafe-get type :documentation)
-      (princ (Struct:unsafe-get type :documentation))
-      (terpri nil t)
-      (terpri))
-    (princ (format Struct:-doc-constructor-properties-line
-                   (Struct:unsafe-get type :name)))
-    (terpri nil t)
-    (terpri)
-    (--each (Struct:unsafe-get type :properties)
-      (-let* (((&plist :name :documentation :default-value
-                       :mutable :type)
-               (Struct:unsafe-properties it)))
-        (princ (format "- %s" name))
-        (when type
-          (princ (format " :: %s" type)))
-        (princ "	(")
-        (when mutable
-          (princ "mutable, "))
-        (princ (format "default: %s)" default-value))
-        (terpri)
-        (when documentation
-          (princ (string-trim documentation))
-          (terpri nil t))
-        (terpri)))
-    (terpri nil t)
-    (princ (format Struct:-doc-constructor-syntax-info
-                   (Struct:unsafe-get type :name)))
-    (terpri nil t)
-    (terpri)
-    (princ (Struct:-doc-constructor-signature type))))
-
-(defun Struct:-doc-constructor-signature (type)
-  (--> (--map
-        (format "%s" (Struct:unsafe-get type :keyword))
-        (Struct:unsafe-get type :properties))
-       (mapconcat #'identity it " ")
-       (format "\(fn (&plist %s))" it)))
-
-(defun Struct:-doc-predicate (type)
-  (with-output-to-string
-    (princ (format Struct:-doc-predicate (Struct:unsafe-get type :name)))
-    (terpri)))
 
 (defun Struct:-expand-syntax (arguments)
   "Expands shorthand and spread-syntax in ARGUMENTS."
@@ -234,7 +159,6 @@ See also `%s'.")
     (when undefine
       (put name Struct:syntax-highlight-symbol nil))))
 
-;;;###autoload
 (defun Struct:undefine (name)
   "Undefine struct NAME.
 
@@ -250,10 +174,6 @@ It does nothing, if NAME does not name a defined struct-type."
     (fmakunbound name)
     (fmakunbound (intern (concat (symbol-name name) "*")))
     (fmakunbound (intern (concat (symbol-name name) "?")))))
-
-;;;###autoload
-
-
 
 (defmacro Struct:lambda (arguments &rest body)
   "Defines a function with some struct related features."
@@ -353,12 +273,6 @@ Throws an error, if ARGUMENTS are incompatible with that macro."
       (car rest)
     (Struct:Type:get type :ensure)
     (apply type rest)))
-
-(define-symbol-prop 'Struct:Type 'function-documentation
-  (Struct:-doc-constructor (Struct:Type:get 'Struct:Type :ensure)))
-
-(define-symbol-prop 'Struct:Property 'function-documentation
-  (Struct:-doc-constructor (Struct:Type:get 'Struct:Property :ensure)))
 
 (provide 'Struct)
 ;;; Struct.el ends here
---
+++ packages/Struct/src/Struct/Doc.el
@@ -0,0 +1,84 @@
+;; -*- lexical-binding: t -*-
+
+(require 'Struct/Primitives)
+
+(defconst Struct:-doc-predicate
+  "Returns `t', if OBJECT is of struct-type `%s'.
+
+Otherwise returns nil, unless ENSURE is non-nil, in which a
+`wrong-type-argument' is signaled.")
+
+(defconst Struct:-doc-constructor-first-line
+  "Constructs a value of struct-type `%s'.")
+
+(defconst Struct:-doc-constructor-properties-line
+  "Struct `%s' has the following properties:")
+
+(defconst Struct:-doc-constructor-syntax-info
+  "The macro-version (with a star appended) of this constructor
+supports shorthand-syntax, i.e. keyword arguments may be omitted, when
+using an identifier eponymous with the property's name. And also
+spread-syntax via the `,@' operator, with other struct values as
+arguments.
+
+See also `%s'.")
+
+(defconst Struct:-doc-constructor-functions-info
+  "The following functions are associated with this struct:")
+
+(defun Struct:-doc-constructor (type)
+  (with-output-to-string
+    (princ (format Struct:-doc-constructor-first-line
+                   (Struct:unsafe-get type :name)))
+    (terpri nil t)
+    (terpri)
+    (when (Struct:unsafe-get type :documentation)
+      (princ (Struct:unsafe-get type :documentation))
+      (terpri nil t)
+      (terpri))
+    (princ (format Struct:-doc-constructor-properties-line
+                   (Struct:unsafe-get type :name)))
+    (terpri nil t)
+    (terpri)
+    (--each (Struct:unsafe-get type :properties)
+      (-let* (((&plist :name :documentation :default-value
+                       :mutable :type)
+               (Struct:unsafe-properties it)))
+        (princ (format "- %s" name))
+        (when type
+          (princ (format " :: %s" type)))
+        (princ "	(")
+        (when mutable
+          (princ "mutable, "))
+        (princ (format "default: %s)" default-value))
+        (terpri)
+        (when documentation
+          (princ (string-trim documentation))
+          (terpri nil t))
+        (terpri)))
+    (terpri nil t)
+    (princ (format Struct:-doc-constructor-syntax-info
+                   (Struct:unsafe-get type :name)))
+    (terpri nil t)
+    (terpri)
+    (princ (Struct:-doc-constructor-signature type))))
+
+(defun Struct:-doc-constructor-signature (type)
+  (--> (--map
+        (format "%s" (Struct:unsafe-get type :keyword))
+        (Struct:unsafe-get type :properties))
+       (mapconcat #'identity it " ")
+       (format "\(fn (&plist %s))" it)))
+
+(defun Struct:-doc-predicate (type)
+  (with-output-to-string
+    (princ (format Struct:-doc-predicate (Struct:unsafe-get type :name)))
+    (terpri)))
+
+(define-symbol-prop 'Struct:Type 'function-documentation
+  (Struct:-doc-constructor (Struct:Type:get 'Struct:Type :ensure)))
+
+(define-symbol-prop 'Struct:Property 'function-documentation
+  (Struct:-doc-constructor (Struct:Type:get 'Struct:Property :ensure)))
+
+(provide 'Struct/Doc)
